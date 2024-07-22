a36f7b3803b8f2bddddfd5e3a10f1983defb0303
Waltson Silva <waltson.dasilva@gmail.com>
2024-07-21 21:21:16 -0300

    merge

--- src/main/java/com/fiap/lanchonete/infrastructure/adapters/customers/repository/CustomerRepository.java
+++ src/main/java/com/fiap/lanchonete/infrastructure/adapters/customers/repository/CustomerRepository.java
@@ -1,27 +1,18 @@
 package com.fiap.lanchonete.infrastructure.adapters.customers.repository;
 
-<<<<<<< HEAD
-import com.fiap.lanchonete.infrastructure.adapters.customers.entity.CustomerEntity;
-=======
-import java.util.Optional;
-import java.util.UUID;
 
->>>>>>> d52793157dc9ffeb982491212156475a06c2977c
+import com.fiap.lanchonete.infrastructure.adapters.customers.entity.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-<<<<<<< HEAD
 import java.util.Optional;
 import java.util.UUID;
-=======
-import com.fiap.lanchonete.infrastructure.adapters.customers.entity.CustomerEntity;
->>>>>>> d52793157dc9ffeb982491212156475a06c2977c
 
 public interface CustomerRepository extends JpaRepository<CustomerEntity, UUID> {
 
     @Query("SELECT c FROM CustomerEntity c WHERE c.document = :document")
-    Optional<CustomerEntity> findCustomer( @Param("document") String document);
+    Optional<CustomerEntity> findCustomer(@Param("document") String document);
 
 
 }
