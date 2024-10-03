863a85bfd9113abe65a87cc4865bcaec6b053701
Fellipe Bastos <fbastos95@gmail.com>
2024-10-02 22:25:22 -0300

    ajustando migration v4

--- src/main/resources/db/migration/mysql/V4__initial_data_orders.sql
+++ src/main/resources/db/migration/mysql/V4__initial_data_orders.sql
@@ -1,5 +1,5 @@
-INSERT INTO orders (id, customer_id, state, total_price, payment_confirmation_status, payment_id, creation_time) VALUES (1, null, 'PENDING', 51.0, 'PENDING', null, '2024-09-15 09:30');
-INSERT INTO orders (id, customer_id, state, total_price, payment_confirmation_status, payment_id, creation_time) VALUES (2, 1, 'PENDING', 67.0, 'PENDING', null, '2024-09-15 15:00');
+INSERT INTO orders (id, customer_id, state, total_price, payment_confirmation_status, payment_id, creation_time, order_number) VALUES (1, null, 'PENDING', 51.0, 'PENDING', null, '2024-09-15 09:30', 1);
+INSERT INTO orders (id, customer_id, state, total_price, payment_confirmation_status, payment_id, creation_time, order_number) VALUES (2, 1, 'PENDING', 67.0, 'PENDING', null, '2024-09-15 15:00', 2);
 
 INSERT INTO order_item (id, product_id, unit_price, quantity, observation, total_price, order_id) VALUES (1, 1, 25.0, 1, null, 25.0, 1);
 INSERT INTO order_item (id, product_id, unit_price, quantity, observation, total_price, order_id) VALUES (2, 4, 12.0, 1, null, 12.0, 1);
@@ -8,4 +8,3 @@ INSERT INTO order_item (id, product_id, unit_price, quantity, observation, total
 
 INSERT INTO order_item (id, product_id, unit_price, quantity, observation, total_price, order_id) VALUES (5, 2, 30.0, 2, null, 60.0, 2);
 INSERT INTO order_item (id, product_id, unit_price, quantity, observation, total_price, order_id) VALUES (6, 7, 7.0, 1, null, 7.0, 2);
-
