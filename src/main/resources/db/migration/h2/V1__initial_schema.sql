a0b8e2fa7b0744251be75526b271e91534063723
Waltson Silva <waltson.dasilva@gmail.com>
2024-10-04 13:23:40 -0300

    implementaçao da migração de banco de dados

--- src/main/resources/db/migration/h2/V1__initial_schema.sql
+++ src/main/resources/db/migration/h2/V1__initial_schema.sql
@@ -1,60 +1,71 @@
-CREATE TABLE IF NOT EXISTS `product` (
-    id BIGINT AUTO_INCREMENT,
+-- Criação da tabela products
+CREATE TABLE IF NOT EXISTS product (
+    id BIGSERIAL,
     description VARCHAR(255) NOT NULL,
     price DECIMAL(19,2) NOT NULL,
     category VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (id)
 );
 
-ALTER TABLE product ALTER COLUMN id RESTART WITH 1000;
+-- Reiniciar a sequência para o campo id
+ALTER SEQUENCE product_id_seq RESTART WITH 1000;
 
-CREATE TABLE IF NOT EXISTS `customer` (
-    id BIGINT AUTO_INCREMENT,
+-- Criação da tabela customer
+CREATE TABLE IF NOT EXISTS customer (
+    id BIGSERIAL,
     name VARCHAR(255) NOT NULL,
     document VARCHAR(255) NOT NULL,
     mail VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (id)
 );
 
-ALTER TABLE customer ALTER COLUMN id RESTART WITH 1000;
+-- Reiniciar a sequência para o campo id
+ALTER SEQUENCE customer_id_seq RESTART WITH 1000;
 
-CREATE TABLE IF NOT EXISTS `payment` (
-    id BIGINT AUTO_INCREMENT,
+-- Criação da tabela payment
+CREATE TABLE IF NOT EXISTS payment (
+    id BIGSERIAL,
     time TIMESTAMP NOT NULL,
     amount DECIMAL(19,2) NOT NULL,
     transaction_id VARCHAR(255) NOT NULL,
     status VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (id)
 );
 
-ALTER TABLE payment ALTER COLUMN id RESTART WITH 1000;
+-- Reiniciar a sequência para o campo id
+ALTER SEQUENCE payment_id_seq RESTART WITH 1000;
 
-CREATE TABLE IF NOT EXISTS `orders` (
-    id BIGINT AUTO_INCREMENT,
-    customer_id BIGINT DEFAULT NULL,
-    state VARCHAR(255) NOT NULl,
+-- Criação da tabela orders
+CREATE TABLE IF NOT EXISTS orders (
+    id BIGSERIAL,
+    customer_id BIGINT,
+    state VARCHAR(255) NOT NULL,
     total_price DECIMAL(19,2) NOT NULL,
     payment_confirmation_status VARCHAR(255) NOT NULL,
-    payment_id BIGINT DEFAULT NULL,
+    payment_id BIGINT,
     creation_time TIMESTAMP NOT NULL,
+    order_number DECIMAL(19,1) NOT NULL,
+    PRIMARY KEY (id),
     FOREIGN KEY (customer_id) REFERENCES customer(id),
-    FOREIGN KEY (payment_id) REFERENCES payment(id),
-    PRIMARY KEY (`id`)
+    FOREIGN KEY (payment_id) REFERENCES payment(id)
 );
 
-ALTER TABLE orders ALTER COLUMN id RESTART WITH 1000;
+-- Reiniciar a sequência para o campo id
+ALTER SEQUENCE orders_id_seq RESTART WITH 1000;
 
-CREATE TABLE IF NOT EXISTS `order_item` (
-    id BIGINT AUTO_INCREMENT,
+-- Criação da tabela order_item
+CREATE TABLE IF NOT EXISTS order_item (
+    id BIGSERIAL,
     product_id BIGINT NOT NULL,
     unit_price DECIMAL(19,2) NOT NULL,
     quantity INT NOT NULL,
     observation TEXT DEFAULT NULL,
     total_price DECIMAL(19,2) NOT NULL,
     order_id BIGINT NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES product (id),
-    FOREIGN KEY (order_id) REFERENCES `orders` (id),
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (id),
+    FOREIGN KEY (product_id) REFERENCES product(id),
+    FOREIGN KEY (order_id) REFERENCES orders(id)
 );
 
-ALTER TABLE order_item ALTER COLUMN id RESTART WITH 1000;
+-- Reiniciar a sequência para o campo id
+ALTER SEQUENCE order_item_id_seq RESTART WITH 1000;
--- src/main/resources/db/migration/h2/V3__initial_data_customers.sql
+++ src/main/resources/db/migration/h2/V3__initial_data_customers.sql
@@ -1,2 +1,3 @@
-INSERT INTO customer (id, name, document, mail) VALUES (1, 'Roberta Carlos', '111.111.111.-11', 'roberto@email.com');
-INSERT INTO customer (id, name, document, mail) VALUES (2, 'Flavia Souza', '222.222.222.-22', 'flavia@email.com');
+-- Inserção de dados na tabela customer
+INSERT INTO customer (id, name, document, mail) VALUES (1, 'Roberta Carlos', '111.111.111-11', 'roberto@email.com');
+INSERT INTO customer (id, name, document, mail) VALUES (2, 'Flavia Souza', '222.222.222-22', 'flavia@email.com');
