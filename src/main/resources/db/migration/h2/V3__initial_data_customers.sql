0cd059761e93369c1e170e479c3b934c9f947a5e
Fellipe Bastos <fbastos95@gmail.com>
2024-10-04 18:01:55 -0300

    ajustando script h2 e application

--- src/main/resources/db/migration/h2/V3__initial_data_customers.sql
+++ src/main/resources/db/migration/h2/V3__initial_data_customers.sql
@@ -1,3 +1,3 @@
 -- Inserção de dados na tabela customer
-INSERT INTO customer (id, name, document, mail) VALUES (1, 'Roberta Carlos', '111.111.111-11', 'roberto@email.com');
-INSERT INTO customer (id, name, document, mail) VALUES (2, 'Flavia Souza', '222.222.222-22', 'flavia@email.com');
+INSERT INTO customer (name, document, mail) VALUES ('Roberta Carlos', '111.111.111-11', 'roberto@email.com');
+INSERT INTO customer (name, document, mail) VALUES ('Flavia Souza', '222.222.222-22', 'flavia@email.com');
---
+++ src/main/resources/db/migration/postgres/V5__create_sequence.sql
@@ -0,0 +1,3 @@
+CREATE SEQUENCE order_sequence
+    START WITH 1
+    INCREMENT BY 1;
