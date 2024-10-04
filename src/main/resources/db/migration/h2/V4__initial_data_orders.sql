73fc471e4bc6c943e3eea8e14a673d2097b8dee2
Waltson Silva <waltson.dasilva@gmail.com>
2024-10-04 16:37:02 -0300

    implementação da migração do banco de dados part 2

--- src/main/resources/db/migration/h2/V4__initial_data_orders.sql
+++ src/main/resources/db/migration/h2/V4__initial_data_orders.sql
@@ -1,9 +1,9 @@
 -- Inserção de dados na tabela orders
 INSERT INTO orders (id, customer_id, state, total_price, payment_confirmation_status, payment_id, creation_time, order_number)
-VALUES (1, null, 'PENDING', 51.0, 'PENDING', null, '2024-09-15 09:30:00', 1);
+VALUES (1, null, 'PENDING', 51.0, 'PENDING', null, '2024-09-15 09:30:00', 100000);
 
 INSERT INTO orders (id, customer_id, state, total_price, payment_confirmation_status, payment_id, creation_time, order_number)
-VALUES (2, 1, 'PENDING', 67.0, 'PENDING', null, '2024-09-15 15:00:00', 2);
+VALUES (2, 1, 'PENDING', 67.0, 'PENDING', null, '2024-09-15 15:00:00', 200000);
 
 -- Inserção de dados na tabela order_item
 INSERT INTO order_item (id, product_id, unit_price, quantity, observation, total_price, order_id)
